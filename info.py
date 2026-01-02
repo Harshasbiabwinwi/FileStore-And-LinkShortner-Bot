c8dbe85c8f99fe8eade461c4daec3c07d82dd09d
Harshasbiabwinwi <harshvardhanprogamingi@gmail.com>
2026-01-02 12:57:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://don:don@cluster0.5atr1ut.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://villainravangaming:mikey_kun_781_@cluster0.fbgs1zz.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "villainravangaming")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Acro_files')
 
 # Others
